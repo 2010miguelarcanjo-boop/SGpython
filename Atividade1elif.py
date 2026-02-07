1eb45700a4a939cd98cb6f57ab66f29b46e309fe
Livelier143 <2010miguelarcanjo@gmail.com>
2026-02-07 17:46:52 -0300

    Add grading script with average calculation
    first version of code

---
+++ Atividade1elif.py
@@ -0,0 +1,11 @@
+nota1=float(input('digite sua nota: ' ))
+nota2=float(input('digite sua nota: ' ))
+nota3=float(input('digite sua nota: ' ))
+media=(nota1+nota2+nota3)/3
+if media<5:
+    print ('reprovado')
+elif media==10:
+    print ('nota perfeita')
+else :
+    print('aprovado')
+
