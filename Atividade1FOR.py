614fa0a2eb90aece71c6f12beb8d15ff538ed610
Livelier143 <2010miguelarcanjo@gmail.com>
2026-08-06 21:44:09 -0300

    Add game preference evaluation in Atividade1FOR.py

---
+++ Atividade1FOR.py
@@ -0,0 +1,10 @@
+jogos=["plants vs zombies","fortnite","roblox","geshin impact"]
+for jogo in jogos:
+    if jogo=="plants vs zombies":
+        print(jogo+" gosto")
+    elif jogo=="fortnite" or jogo=="roblox":
+        print (jogo+" razoavel")
+    else:
+        print(jogo+" nao curti")
+
+        
